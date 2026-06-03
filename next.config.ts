04effd39c4075177d8f7121a721ad4445206f845
zhadya <zaid.omar962@gmail.com>
2026-06-03 11:12:42 +0400

    Fix the root path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isProd ? '/zoro' : '',
+  assetPrefix: isProd ? '/zoro/' : '',
   devIndicators: false,
   images: {
     unoptimized: true,
