5c608b037841d6151d5dfbeaaa2bf99388215d13
zoro421 <zoro421@users.noreply.github.com>
2026-06-04 07:27:51 +0000

    deploy: d1a24a23c188ad59ea65928b302ff86f2b480ac1

--- _next/static/a2FsPA33-XSekm4PtgZE2/_buildManifest.js
+++ _next/static/a2FsPA33-XSekm4PtgZE2/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/zoro//_next/:path+",
-        "destination": "/zoro/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
