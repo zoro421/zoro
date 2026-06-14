4d943db5d17c2db4d1023da68bab866b06456a85
zhadya <zaid.omar962@gmail.com>
2026-06-14 17:23:47 +0400

    logo fix

--- lib/brand.ts
+++ lib/brand.ts
@@ -1,7 +1,7 @@
 export const BRAND = {
-  name: process.env.NEXT_PUBLIC_APP_NAME!,
-  nameAr: process.env.NEXT_PUBLIC_APP_NAME_AR!,
-  email: process.env.NEXT_PUBLIC_APP_EMAIL!,
-  url: process.env.NEXT_PUBLIC_APP_URL!,
-  domain: process.env.NEXT_PUBLIC_APP_DOMAIN!,
+  name: process.env.NEXT_PUBLIC_APP_NAME ?? 'Ozwati',
+  nameAr: process.env.NEXT_PUBLIC_APP_NAME_AR ?? 'عزوتي',
+  email: process.env.NEXT_PUBLIC_APP_EMAIL ?? 'info@ozwati.com',
+  url: process.env.NEXT_PUBLIC_APP_URL ?? 'https://www.ozwati.com',
+  domain: process.env.NEXT_PUBLIC_APP_DOMAIN ?? 'www.ozwati.com',
 }
